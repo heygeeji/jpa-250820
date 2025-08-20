f010eb5a8d846ffe9efb4e25c5d5e1e5757d5216
heygeeji <heygeeji@gmail.com>
2025-08-20 15:14:57 +0900

    create and use Post class instead of WiseSaying class

--- src/main/java/com/example/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/example/domain/wiseSaying/entity/Post.java
@@ -1,23 +1,21 @@
 package com.example.domain.wiseSaying.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
 @Entity
-public class wiseSaying {
+public class Post {
     @Id //jpa는 식별자가 반드시!! 필요
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY) //autoincrement
-    private int id; //Int
+    private int id;
     private String saying; //varchar(255)
-    private String author; //varchar(255)
-}
 
+    @Column(columnDefinition = "TEXT") //안하면 default varchar(255)
+    private String author;
+
+
+}
 
 //class article { } -> jpa -> create table article()
