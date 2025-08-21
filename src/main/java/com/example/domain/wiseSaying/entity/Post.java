3298039865df2daee06eb5b0d3416fe0350e8526
heygeeji <heygeeji@gmail.com>
2025-08-21 11:26:12 +0900

    Add @NoArgsConstructor to generate default constructor

--- src/main/java/com/example/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/example/domain/wiseSaying/entity/Post.java
@@ -5,34 +5,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Entity
 @Setter
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class Post {
     @Id //jpa는 식별자가 반드시!! 필요
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY) //autoincrement
     private int id;
-    private final String title; //varchar(255)
+    private String title; //varchar(255)
 
     @Column(columnDefinition = "TEXT") //안하면 default varchar(255)
-    private final String content;
+    private String content;
     //제목, 내용은 수정 가능성이 있기 때문에 final은 안쓰는게 맞음.
 
-    public Post(){
-        this.title="";
-        this.content = "";
-    } //기본 생성자 ->Post 빨간 줄 사라짐
+//    public Post(){} //기본 생성자 ->Post 빨간 줄 사라짐
 //    //Jpa에서는 기본생성자 요구
-//
-//    public Post(String title, String content) {
-//        this.title = title;
-//        this.content = content;
-//    }
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
 
 }
